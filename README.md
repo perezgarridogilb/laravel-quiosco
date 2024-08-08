7202e082918387c6b144f0c4843e32a089416733
Gilberto <Betho6990@gmail.com>
2024-08-08 16:37:45 -0600

    adding logo

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # laravel-quiosco
 fullstack project using laravel and react
+
+´´´
+# cosas de tailwindcss para dev
+ npm install -D tailwindcss postcss autoprefixer
+´´´
+
+´´´
+# cosas "para rutas"
+ npm i react-router-dom
+´´´
+
+snnipet: rfc + tab, retorna la estructura básica de un componente de react
+
+vite es una forma moderna de crear proyectos con react, permite en diferentes opciones
+
+swc es para deploy, 
+
+se seleccionó javascript en la opción de terminal
--- react-quiosco/src/router.jsx
+++ react-quiosco/src/router.jsx
@@ -1,10 +1,43 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import AuthLayout from "./layouts/AuthLayout";
+import Inicio from "./views/Inicio";
+import Login from "./views/Login";
+import Registro from "./views/Registro";
 
 const router = createBrowserRouter([
     {
+        /** página principal */
         path: '/',
-        element: <Layout />
+        /** componente de react cuando visite esa página principal */
+        /** un layout es para reutilizar */
+        element: <Layout />,
+        /** los que estén aquí definidos como sus hijos van a utilizar este layout */
+        children: [
+            {
+                index: true,
+                element: <Inicio />
+            }
+        ]
+    },
+    {
+        /** página principal */
+        path: '/auth',
+        /** componente de react cuando visite esa página principal */
+        /** un layout es para reutilizar */
+        element: <AuthLayout />,
+        children: [
+            {
+                path: '/auth/login',
+                /* index: true, */
+                element: <Login />
+            },
+            {
+                path: '/auth/registro',
+                /* index: true, */
+                element: <Registro />
+            }
+        ]  
     }
 
 ])
