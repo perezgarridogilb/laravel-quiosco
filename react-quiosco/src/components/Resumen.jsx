7081868f994d604fb678720fa64fd95873c2df51
Gilberto <Betho6990@gmail.com>
2024-08-30 17:04:10 -0600

    Getting and Displaying Categories

---
+++ react-quiosco/src/components/Resumen.jsx
@@ -0,0 +1,8 @@
+
+export default function Resumen() {
+  return (
+    <div className="w-72">
+      Resumen
+    </div>
+  )
+}
--- react-quiosco/src/layouts/Layout.jsx
+++ react-quiosco/src/layouts/Layout.jsx
@@ -1,11 +1,23 @@
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
+import Resumen from '../components/Resumen'
+
+/** Hubo preferencia por acomodar en orden:
+ *  Dependencias, o hubs de react, componentes, después funciones y finalmente hojas de estilo en css */
 
 export default function Layout() {
   return (
-    <div>
-      Layout
-
+    /**
+     * izquierda a derecha en un tamaño mediano: 'md:flex'
+     */
+    <div className='md:flex'>
+      <Sidebar />
+      <main className='flex-1'>
       <Outlet />
+      </main>
+      
+
+      <Resumen />
       </div>
   )
 }
