2e5db223d4a46f742c8a3bd36f4ab9b01b42b9d1
Gilberto <Betho6990@gmail.com>
2024-09-02 15:25:15 -0600

    analytics

--- react-quiosco/src/components/Sidebar.jsx
+++ react-quiosco/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import Categoria from "./Categoria";
 import { categorias } from "../data/categorias"
 
 export default function Sidebar() {
@@ -7,10 +8,25 @@ export default function Sidebar() {
         <img className="w-40" alt="" src="img/logo.svg" />
       </div>
 
+      {/* implicito, paréntesis de react */}
       <div className="mt-10">
+        {/*         
         {categorias.map( categoria => (
             <p>{categoria.nombre}</p>
-        ))}
+        ))} */}
+
+        {/* explicito */}
+
+        {categorias.map(categoria => {
+            // Por cada objeto 'categoria' en el array 'categorias',
+            // realiza lo siguiente:
+            
+            // 1. Extraer el valor de la propiedad 'nombre' del objeto 'categoria'.
+            const nombreCategoria = categoria.nombre;
+            
+            // 2. Crear un elemento <p> y colocar el nombre de la categoría dentro de él.
+            return <p>{nombreCategoria}</p>;
+        })}
       </div>
     </aside>
   )
